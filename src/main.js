c70028a959010d2cf75efbe50cc59c3da8b7eb4b
E2029-Aydin <aydinsanoz2@gmail.com>
2021-01-21 19:26:05 +0300

    first init of bookStore

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const Book = (props) => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Welcome Book</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Book;
